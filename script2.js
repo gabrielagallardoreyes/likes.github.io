0e5a89c63b4f7656297c9b103ed53428b317be9e
Gabriela Gallardo Reyes <148491870+gabrielagallardoreyes@users.noreply.github.com>
2024-01-05 11:32:15 -0300

    Likes

---
+++ script2.js
@@ -0,0 +1,23 @@
+var likes = 9;
+
+function me_gusta(){
+    likes++;
+    var elemento_numero = document.querySelector("#number");
+    elemento_numero.innerText = likes;
+}
+
+var likes2 = 12;
+
+function me_gusta1(){
+    likes2++;
+    var elemento_numero = document.querySelector("#number2");
+    elemento_numero.innerText = likes2;
+}
+
+var likes3 = 9;
+
+function me_gusta2(){
+    likes3++;
+    var elemento_numero = document.querySelector("#number3");
+    elemento_numero.innerText = likes3;
+}
